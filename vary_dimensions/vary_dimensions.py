b5c048b134427630845d3e415f8e520256a87d81
BudMacaulay <55360869+BudMacaulay@users.noreply.github.com>
2021-03-19 10:15:18 +0000

    Add files via upload
    Addind a short function to allow swift changing of lattice dimensions. Will add a wrapper to allow easy CLI shortly.

---
+++ vary_dimensions/vary_dimensions.py
@@ -0,0 +1,25 @@
+from pymatgen.core import Lattice, Structure
+
+def vary_lattice_dimensions(input_structure, varyamount=0.10, a=True, b=True, c=False):
+    """ a tool to vary the lattice dimensions for a completed vasp run with the intent to check ISIF=3 and therefore
+    not be so paranoid"""
+
+    structure = Structure.from_file(input_pos)
+    structure_new = structure.copy()
+    a_var = 1
+    b_var = 1
+    c_var = 1
+    if a:
+        a_var +=varyamount
+    if b:
+        b_var += varyamount
+    if c:
+        c_var =+ varyamount
+    structure_new.lattice = Lattice.from_parameters(a=a_var*structure.lattice.a,
+                                                    b=b_var*structure.lattice.b,
+                                                    c=c_var*structure.lattice.c,
+                                                    alpha=structure.lattice.alpha,
+                                                    beta=structure.lattice.beta,
+                                                    gamma=structure.lattice.gamma)
+    return structure_new
+
