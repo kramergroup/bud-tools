fc82259bdff71bfc2ae57cd5f3dc5f8bf309b75f
BudMacaulay <55360869+BudMacaulay@users.noreply.github.com>
2021-03-23 11:09:48 +0000

    Update vary_dimensions.py
    Cleanup and header.

--- vary_dimensions/vary_dimensions.py
+++ vary_dimensions/vary_dimensions.py
@@ -1,21 +1,20 @@
 from pymatgen.core import Lattice, Structure
+from pymatgen.io.xyz import XYZ
 
 
-def vary_lattice_dimensions(input_structure, varyamount=0.10, a=True, b=True, c=False):
+def vary_dimensions(input_structure, varyamount=0.10, a=True, b=True, c=False):
     """ a tool to vary the lattice dimensions for a completed vasp run with the intent to check ISIF=3 and therefore
     not be so paranoid"""
 
-    structure = Structure.from_file(input_pos)
+    structure = Structure.from_file(input_structure)
     structure_new = structure.copy()
     a_var = 1
     b_var = 1
     c_var = 1
-    if a:
-        a_var += varyamount
-    if b:
-        b_var += varyamount
-    if c:
-        c_var = + varyamount
+
+    if a: a_var += varyamount
+    if b: b_var += varyamount
+    if c: c_var += varyamount
     structure_new.lattice = Lattice.from_parameters(a=a_var * structure.lattice.a,
                                                     b=b_var * structure.lattice.b,
                                                     c=c_var * structure.lattice.c,
